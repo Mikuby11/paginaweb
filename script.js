51330d25c12fbafa19aef44df293fb189618f865
Fernanda Humeres <fda.humeres.lo@gmail.com>
2025-11-29 00:31:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -198,23 +198,26 @@ if (document.getElementById('graficoBarras')) {
 
 // Datos de carruseles para laboratorio.html
 const carruseles = [
-    {
-        id: "carrusel1",
-        slides: [
-            { img: "img/1.webp", titulo: "Proyecto 1", texto: "Descripción del primer proyecto." },
-            { img: "img/2.webp", titulo: "Proyecto 2", texto: "Descripción del segundo proyecto." },
-            { img: "img/3.webp", titulo: "Proyecto 3", texto: "Descripción del tercer proyecto." }
-        ]
-    },
-    {
-        id: "carrusel2",
-        slides: [
-            { img: "img/4.webp", titulo: "Proyecto 4", texto: "Este es otro carrusel de ejemplo." },
-            { img: "img/5.webp", titulo: "Proyecto 5", texto: "Puedes agregar cuantos carruseles quieras." }
-        ]
-    }
-];
-
+                {
+                    id: "carrusel1",
+                    slides: [
+                        { img: "img/1.webp", titulo: "Servicio rápido", texto: "Tus cotizaciones en menos de 24 horas con entregas de hasta 48 horas hábiles" },
+                        { img: "img/2.webp", titulo: "De lunes a sábado", texto: "Respuesta de lunes a sábado con posibilidad de entrega en metros o retiro" },
+                        { img: "img/3.webp", titulo: "Modelado trabajado en Fusión 360", texto: "Ideal para trabajos que requieran precisión y medidas estricas, para sistemas y artefactos." }
+                    ]
+                },
+                {
+                    id: "carrusel2",
+                    slides: [
+                        { img: "img/4.webp", titulo: "Trabajo 1", texto: "Cuchillo como prototipo estético" },
+                        { img: "img/5.webp", titulo: "Trabajo 2", texto: "Monedas tipo tazos para pokemon TCG personalizadas"},
+                        { img: "img/6.webp", titulo: "Trabajo 3", texto: "Contramoldes para pellets de comida de mascotas" },
+                        { img: "img/7.webp", titulo: "Trabajo 4", texto: "Contenedor para acuarelas modular e intercambiable" },
+                        { img: "img/8.webp", titulo: "Trabajo 5", texto: "Contenedor de pellets biofertilizantes ajustable a mangueras de riego" },
+                        { img: "img/9.webp", titulo: "Trabajo 6", texto: "Componentes de espada de Giyuu Tomioka para Cosplay" }
+                    ]
+                }
+            ];
 function crearCarrusel(carruselData) {
     const { id, slides } = carruselData;
 
